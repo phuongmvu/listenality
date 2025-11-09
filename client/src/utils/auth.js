46a7f94cf9ad36255b93dce2cd8acfa16fdcdd18
phuong <47091041+phuongmvu@users.noreply.github.com>
2025-11-09 17:47:46 -0500

    Update auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,16 +1,24 @@
-export const getTokenFromUrl = () => {
-  return window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce((initial, item) => {
-      let parts = item.split('=')
-      initial[parts[0]] = decodeURIComponent(parts[1])
-      return initial
-    }, {})
-}
-
 export const loginUrl = () => {
-  const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:3001'
-  return `${API_URL}/api/auth/spotify/login`
-}
+  const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+  const redirectUri =
+    import.meta.env.VITE_SPOTIFY_REDIRECT_URI ||
+    `${window.location.origin}/api/auth/spotify/callback`
+  const scopes = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'user-read-recently-played',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative'
+  ]
 
+  const url = new URL('https://accounts.spotify.com/authorize')
+  url.searchParams.set('response_type', 'code')
+  url.searchParams.set('client_id', clientId)
+  url.searchParams.set('scope', scopes.join(' '))
+  url.searchParams.set('redirect_uri', redirectUri)
+  url.searchParams.set('state', Math.random().toString(36).slice(2))
+
+  return url.toString()
+}
